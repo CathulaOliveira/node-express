fb9ac3ed0766db1595c8eb3e2cb2e8beedb0f10f
cathula.oliveira <cathulapameladeoliveira@gmail.com>
2023-03-20 19:14:49 -0300

    router

--- src/routes/router.js
+++ src/routes/router.js
@@ -20,4 +20,45 @@ router.post('/users', (req, res) => {
     });
 });
 
+router.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+
+  User.findByPk(userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+
+      res.json(user);
+    })
+    .catch(err => {
+      res.status(400).json({ message: err.message });
+    });
+});
+
+router.put('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const { name, email, password } = req.body;
+
+  User.findByPk(userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+
+      user.name = name;
+      user.email = email;
+      user.password = password;
+
+      return user.save();
+    })
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => {
+      res.status(400).json({ message: err.message });
+    });
+});
+
+
 module.exports = router;
